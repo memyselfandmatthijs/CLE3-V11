0d6eedaec700198fb8a7e1a6a9eb7cd475129ec8
Rav1sh <ravishsomai808@gmail.com>
2021-03-13 12:49:26 +0100

    Added DOM variables for blackjack

---
+++ decks/lower/blackjack.js
@@ -0,0 +1,5 @@
+// DOM variables
+let startGame = document.getElementById("start-game");
+let hitButton = document.getElementById("hit");
+let stayButton = document.getElementById("stay");
+let doubleDownButton = document.getElementById("double-down")
